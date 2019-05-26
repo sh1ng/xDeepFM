35e248abb7407005a6a9066408477504d4150c22
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-05-26 20:56:30 +0200

    fix convert to ffm

--- exdeepfm/convert_ffm_process.py
+++ exdeepfm/convert_ffm_process.py
@@ -35,8 +35,8 @@ sample(input_path, train_file, eval_file)
 # 统计category feat的词频
 feat_cnt = defaultdict(lambda: 0)
 """
-def scan(filename, feat_cnt):
-    for row in csv.DictReader(open(filename)):
+def scan(filename, feat_cnt, fieldnames):
+    for row in csv.DictReader(open(filename), dialect='excel-tab', fieldnames=fieldnames):
         for key, val in row.items():
             if 'C' in key:
                 if val == '':
@@ -52,9 +52,10 @@ scan(test_file)
 T = 4 
 """
 #考虑连续特征离散化和长尾特征之后，统计训练集和测试集中的feat
-def get_feat(featSet, filename):
-    for row in csv.DictReader(open(filename)):
+def get_feat(featSet, filename, fieldNames):
+    for row in csv.DictReader(open(filename), dialect='excel-tab',  fieldnames=fieldNames):
         for key, val in row.items():
+            # print(key, val)
             if 'I' in key and key != "Id":
                 if val == '':
                     featSet.add(str(key) + '#' + 'absence')
@@ -101,9 +102,9 @@ for index, field in enumerate(fieldList, start=1):
 print('field dict num:', len(fieldIndex))
 """
 
-def convert_to_ffm(src_path, dst_path, fieldIndex, featIndex):
+def convert_to_ffm(src_path, dst_path, fieldIndex, featIndex, fieldNames):
     out = open(dst_path, 'w')
-    for row in csv.DictReader(open(src_path)):
+    for row in csv.DictReader(open(src_path), dialect='excel-tab', fieldnames=fieldNames):
         feats = []
         feats.append(row['Label'])
         for key, val in row.items():
@@ -149,40 +150,49 @@ tr_dst = './300w_test_T4.process.new.ffm'
 convert_to_ffm(tr_src, tr_dst, featIndex)
 """
 if __name__ == '__main__':
-    input_path = "../input.csv"
+    input_path = "../../arboretum_benchmark/data/dac/train.txt"
     train_file = "../train.csv"
     eval_file = "../eval.csv"
     test_file = "../test.csv"
     sample(input_path, train_file, eval_file, test_file)
+
+    fieldList = ['I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13', 'C1', 'C2', 'C3',
+                 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18',
+                 'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26']
+
+    fields = ['Label']
+    fields.extend(fieldList)
+
     # 统计category feat的词频
     feat_cnt = defaultdict(lambda: 0)
-    scan(train_file, feat_cnt)
-    scan(eval_file, feat_cnt)
-    scan(test_file, feat_cnt)
+    scan(train_file, feat_cnt, fields)
+    scan(eval_file, feat_cnt, fields)
+    scan(test_file, feat_cnt, fields)
     # 离散特征判断为长尾的阈值
     T = 4
+
+
+
     featSet = set()
-    get_feat(featSet, train_file)
-    get_feat(featSet, test_file)
-    get_feat(featSet, eval_file)
+    get_feat(featSet, train_file, fields)
+    get_feat(featSet, test_file, fields)
+    get_feat(featSet, eval_file, fields)
 
     print('train and test data total feat num:', len(featSet))
 
     featIndex = dict()
     for index, feat in enumerate(featSet, start=1):
         featIndex[feat] = index
-        # print(index, feat)
+        print(index, feat)
     print('feat dict num:', len(featIndex))
 
     fieldIndex = dict()
-    fieldList = ['I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13', 'C1', 'C2', 'C3',
-                 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18',
-                 'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26']
+
 
     for index, field in enumerate(fieldList, start=1):
         fieldIndex[field] = index
     print('field dict num:', len(fieldIndex))
     
-    convert_to_ffm(train_file, "../train.ffm", fieldIndex, featIndex)
-    convert_to_ffm(eval_file, "../eval.ffm", fieldIndex, featIndex)
-    convert_to_ffm(test_file, "../test.ffm", fieldIndex, featIndex)
+    convert_to_ffm(train_file, "../train.ffm", fieldIndex, featIndex, fields)
+    convert_to_ffm(eval_file, "../eval.ffm", fieldIndex, featIndex, fields)
+    convert_to_ffm(test_file, "../test.ffm", fieldIndex, featIndex, fields)
